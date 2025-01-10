5539f0a7823cf01ed98568b3a2720e038b1afea6
Brent Lineberry <brent@thelineberrys.com>
2025-01-09 20:11:38 -0500

    reset init

--- djangomicropubpoc/urls.py
+++ djangomicropubpoc/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for base project.
+URL configuration for djangomicropubpoc project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
