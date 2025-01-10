15057fb7cd315d439b133034283d7569b0661223
Brent Lineberry <brent@thelineberrys.com>
2025-01-09 20:40:32 -0500

    index.html

--- djangomicropubpoc/settings.py
+++ djangomicropubpoc/settings.py
@@ -36,6 +36,7 @@ if RENDER_EXTERNAL_HOSTNAME:
 # Application definition
 
 INSTALLED_APPS = [
+    'myapp.apps.MyAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
